6f9406384cc1285c41ab7096619be58ef9fd8181
Leonardo yuki <134506662+LeonardoTsuyoshiYuki@users.noreply.github.com>
2025-05-27 16:25:50 -0400

    Register User Valid / Login

--- app/urls.py
+++ app/urls.py
@@ -5,11 +5,13 @@ from django.conf.urls.static import static
 from cars.views import cars_view
 from cars.views import create_cars_view
 from user.views import register_view
+from user.views import login_view
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
+path('register/', register_view, name='register'),
     path('cars/', cars_view, name = 'cars_list'),
     path('create_car/', create_cars_view, name = 'create_car')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
